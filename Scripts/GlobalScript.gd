46c08c7bf79d53b0291f1635fa8079a6340a8f75
Deeeeelan <108163886+Deeeeelan@users.noreply.github.com>
2024-10-27 16:14:10 -0700

    Cleaned up code

--- Scripts/GlobalScript.gd
+++ Scripts/GlobalScript.gd
@@ -5,7 +5,6 @@ var FullScreen = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	pass # Replace with function body.
 	
 func _input(event):
