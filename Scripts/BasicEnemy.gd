dd3ddfa0d7049204a0d68548fc0a975e4feb0f2c
Deeeeelan <108163886+Deeeeelan@users.noreply.github.com>
2024-10-30 18:24:07 -0700

    Reverted Enemy back to CharacterBody

--- Scripts/BasicEnemy.gd
+++ Scripts/BasicEnemy.gd
@@ -1,4 +1,3 @@
-extends CharacterBody3D
 
 
 const SPEED = 5.0
@@ -10,13 +9,8 @@ var lerp_speed = 10
 
 var direction = Vector3.ZERO
 
-
-
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
 	# Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -24,13 +18,6 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Vector2(0,0)
 	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta*lerp_speed)
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
